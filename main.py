6de2ca2138819eaf7fae82df90a2762eaa496260
Vebstere <mittalvaibhav277@gmail.com>
2025-06-02 21:22:57 +0530

    hh

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from collections import defaultdict
 import json
 from MongoDB_connect import MONGODBHANDLER
 from postgresql_connector import POSTGRESQLHANDLER
+from hive import HIVEHANDLER
 from parse_testcase import parse_testcase_file
 from parse_testcase_multithreading import parse_testcase_file_multithreading
 
 
 # mongo_handler = MongoDBHandler()
 
-Databases=["MONGODB", "POSTGRESQL"]
+Databases=["MONGODB", "POSTGRESQL", "HIVE"]
 primary_keys = []
 
 # Initialize 2D dictionaries using defaultdict
